6a641adc1ec569d6396d5860a7d4c58c59c09584
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2021-06-21 16:45:57 +0530

    created home-page

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ app = express();//contains all the functions of express
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
-
+//setting the static folder( The static is a middleware)
+app.use(express.static(process.cwd() + '/public/'));
+// process.cwd() returns the current working directory
+// __dirname return the directory name of the directory containing the JS source code file
 app.get('/',(req,res)=>{
     res.render('home');
 });
+
 
 //setting template engine
 app.use(expressLayout);
