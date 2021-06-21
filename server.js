52d4bc6f5a7ded30729f6cfcd794315a937712d5
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2021-06-21 22:04:14 +0530

    added login and register pages

--- server.js
+++ server.js
@@ -7,16 +7,28 @@ const path = require('path');
 app.use(express.static(process.cwd() + '/public/'));
 // process.cwd() returns the current working directory
 // __dirname return the directory name of the directory containing the JS source code file
-app.get('/',(req,res)=>{
-    res.render('home');
-});
-
 
 //setting template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+// Imp the routes should come after this view engine or else it will not render the site
 
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/cart',(req,res)=>{
+    res.render('customer/cart');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+});
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+});
 
 const PORT = process.env.PORT || 3000;
 
